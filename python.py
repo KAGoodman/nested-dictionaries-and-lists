5761f915909421d416f93c7b1e340182f9a1d7af
kgoodman <kellangoodman@gmail.com>
2022-09-04 12:03:38 -0700

    nested dictionaries

---
+++ python.py
@@ -0,0 +1,56 @@
+x = [ [5,2,3], [10,8,9] ] 
+students = [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 20} ]
+
+x[1][0] = 15
+print(x)
+
+students[0]['last_name'] = 'Bryant'
+print(students)
+
+sports_directory['soccer'][0] = 'Andres'
+print(sports_directory['soccer'])
+
+z[0]['y'] = 30
+print(z)
+
+students = [
+    {'first_name':  'Michael', 'last_name' : 'Jordan'},
+    {'first_name' : 'John', 'last_name' : 'Rosales'},
+    {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+    {'first_name' : 'KB', 'last_name' : 'Tonel'}
+]
+
+def iterateDictionary(some_list):
+    for v in range(0, len(some_list)):
+        names = ""
+        for key,val in some_list[v].items():
+            names += f" {key} - {val}"
+        print(names)
+
+iterateDictionary(students)
+
+dojo = {
+    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+
+def print_info(list):
+    for key,val in list.items():
+        print("")
+        print(f"{len(val)} {key.upper()}")
+        for i in range(0, len(val)):
+            print(val[i])
+
+print_info(dojo)
+
+
+
+
